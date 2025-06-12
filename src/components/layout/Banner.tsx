1b0be2a99ebd24f3bd5645c5ea38cdd8fcfbb216
Suleman Altaf <saksak218@gmail.com>
2025-06-12 13:56:43 +0500

    Add click outside functionality on modal in banner

--- src/components/layout/Banner.tsx
+++ src/components/layout/Banner.tsx
@@ -2,13 +2,38 @@
 
 import { Calendar, Phone, X } from "lucide-react";
 import { motion } from "motion/react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import ContactForm from "../ContactForm";
 
 export default function Banner() {
   const [isOpen, setIsOpen] = useState(false);
   const [isModal, setIsModal] = useState(false);
+
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
+        setIsModal(false);
+      }
+    };
+
+    const handleEscapeKey = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setIsModal(false);
+      }
+    };
+
+    if (isModal) {
+      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener("keydown", handleEscapeKey);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, [isModal]);
 
   return (
     <div className="hidden lg:flex justify-end items-center gap-x-6 bg-gray-900 px-10 py-0.5">
@@ -62,7 +87,10 @@ export default function Banner() {
       {isModal && (
         <div className="z-50 fixed inset-0 min-h-screen">
           <div className="flex justify-center items-center bg-gray-900/40 backdrop-blur-xs min-h-screen">
-            <div className="relative bg-white px-12 py-4 rounded-md w-full max-w-2xl">
+            <div
+              ref={modalRef}
+              className="relative bg-white px-12 py-4 rounded-md w-full max-w-2xl"
+            >
               <h2 className="mt-6 font-semibold text-2xl">Get In Touch</h2>
               <div className="mt-2 pb-8">
                 <ContactForm />
