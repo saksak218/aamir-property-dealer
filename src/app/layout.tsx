2aad3b82b3884280146023ac0d18d9eaf72a3f6e
Suleman Altaf <saksak218@gmail.com>
2025-06-15 13:30:59 +0500

    Remove unused fonts in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 
 import { ClerkProvider } from "@clerk/nextjs";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
